1c15231398ec305d646dc139c2f8eda8b63068ee
Nate Dimock <natedimock@yahoo.com>
2017-12-22 16:50:10 -0600

    Added first half of buttons, made keypress not affect other button's positions

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,45 @@
-const test = document.querySelector("#test");
+const aButton = document.querySelector("#button-a");
+const sButton = document.querySelector("#button-s");
+const dButton = document.querySelector("#button-d");
+const fButton = document.querySelector("#button-f");
+
+const KEYCODE_A = 65;
+const KEYCODE_S = 83;
+const KEYCODE_D = 68;
+const KEYCODE_F = 70;
 
 document.body.addEventListener("keydown", function(e) {
-    if(e.keyCode == 65) {
-        test.classList.add("highlighted");
+    if (e.keyCode == KEYCODE_A) {
+        aButton.classList.add("highlighted");
+    } else if (e.keyCode == KEYCODE_S) {
+        sButton.classList.add("highlighted");
+    } else if (e.keyCode == KEYCODE_D) {
+        dButton.classList.add("highlighted");
+    } else if (e.keyCode == KEYCODE_F) {
+        fButton.classList.add("highlighted");
     }
 });
 
 document.body.addEventListener("keyup", function(e) {
-    if(e.keyCode == 65) {
-        test.classList.remove("highlighted");
+    if (e.keyCode == KEYCODE_A) {
+        aButton.classList.remove("highlighted");
+    } else if (e.keyCode == KEYCODE_S) {
+        sButton.classList.remove("highlighted");
+    } else if (e.keyCode == KEYCODE_D) {
+        dButton.classList.remove("highlighted");
+    } else if (e.keyCode == KEYCODE_F) {
+        fButton.classList.remove("highlighted");
     }
 });
+
+/*document.body.addEventListener("keydown", function(e) {
+    if (e.keyCode == KEYCODE_S) {
+        sButton.classList.add("highlighted");
+    }
+});*/
+
+/*document.body.addEventListener("keyup", function(e) {
+    if (e.keyCode == KEYCODE_S) {
+        sButton.classList.remove("highlighted");
+    }
+});*/
